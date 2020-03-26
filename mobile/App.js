f68bfc7daa051c9d6b51ae23ef650f240dfa9fda
Alex <alex.adaumi@gmail.com>
2020-03-26 16:04:55 -0300

    fourth day

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+import React from "react";
+import { StatusBar } from "react-native";
+
+import Routes from "./src/routes";
+
+export default function App() {
+  return (
+    <>
+      <Routes />
+      <StatusBar backgroundColor="#e02041" barStyle="dark-content" />
+    </>
+  );
+}
